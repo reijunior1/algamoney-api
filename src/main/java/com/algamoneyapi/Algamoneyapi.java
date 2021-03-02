0e689c97e9a4513fbbff1e14223796be1bc5d850
Reinaldo Couto <reijunior1@hotmail.com>
2021-03-02 17:12:08 -0300

    configurando-postgres

---
+++ src/main/java/com/algamoneyapi/Algamoneyapi.java
@@ -0,0 +1,13 @@
+package com.algamoneyapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Algamoneyapi {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Algamoneyapi.class, args);
+	}
+
+}
